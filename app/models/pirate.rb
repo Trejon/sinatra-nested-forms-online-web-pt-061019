00e1fd676a6d9333b533527f8fb0df96e3cadadc
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-09-24 23:48:10 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  
+  @@all = []
+  
+  def initialize(pirate_params) 
+    @name = pirate_params[:name] 
+    @weight = pirate_params[:weight]
+    @height = pirate_params[:height]
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end
